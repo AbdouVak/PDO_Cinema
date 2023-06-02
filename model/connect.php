2d00699ffd0f3f0a7795b1c024e88690513701d2
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-02 16:41:35 +0200

    changment du nomù de la fonction pour une meilleur compréhention

--- model/connect.php
+++ model/connect.php
@@ -9,13 +9,12 @@ abstract class Connect {
     const USER = "root";
     const PASS = "";
 
-    public static function seConnect() {
+    public static function seConnecter() {
         try {
             return new \PDO(
-                "mysql:host=".self::HOST.";dbname=".self::DB.";charset=utf8",self::USER,self::PASS
-            );
-    } catch (\PDOException $ex) {
-        return $ex->getMessage();
-    }
+                "mysql:host=".self::HOST.";dbname=".self::DB.";charset=utf8",self::USER,self::PASS);
+        } catch (\PDOException $ex) {
+            return $ex->getMessage();
+        }
     }
 }
