048e93de4b743ad0ab0e30d72b9a2dcc36bf8191
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-05 16:12:33 +0200

    commentaire

--- view/listFilms.php
+++ view/listFilms.php
@@ -4,11 +4,13 @@
 
 
 <div><?php
+    // boucle pour recupere les info du film. Chaque boucle est un film different
     foreach($requete->fetchAll() as $film){ 
         ?>
         <div>
             <p><?= $film["titre"] ?>-<?= $film["anneeSortieFrance"]?> </p>
-            <a href="index.php?action=description&id= ".<?=$film["id_film"]?>.""><img src="public/img/test_img.jpg" width="200"></a>
+            <!-- quand no va appuyer sur l'image redirige vers l'index avec l'action description et id_film du film de la boucle-->
+            <?= "<a href='index.php?action=description&id= ".$film['id_film']." '><img src='public/img/test_img.jpg' width='200'></a>"?>
         </div>
 <?php } ?></div>
 
