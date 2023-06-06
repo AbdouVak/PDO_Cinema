3322daf652223ca5696de8421d15939ebd35557a
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-06 14:19:27 +0200

    ajouterCastingPage: amélioration des commentaires du code

--- view/ajouterCastingPage.php
+++ view/ajouterCastingPage.php
@@ -3,9 +3,9 @@
 
 <!-- formulaires -->
 <form action="index.php?action=ajouterCasting" method="POST" enctype="multipart/form-data">
-    <!-- +--------------------+ -->
-    <!-- |   Film :  [   \/]  | -->
-    <!-- +--------------------+ -->
+    <!-- +-------------------------------+ -->
+    <!-- |    Film :  [   \/]            | -->
+    <!-- +-------------------------------+ -->
     <label for="film">Film:</label>
     <select name="film"><?php 
         // boucle pour affiche les acteur un par un
@@ -15,15 +15,15 @@
     </select><br><br>
 
     <!-- +-------------------------------+  -->
-    <!-- |   Role :  [              ]    | -->
+    <!-- |    Role :  [              ]   | -->
     <!-- +-------------------------------+  -->
     <label for="role">Role:</label>
     <input type="text" name="role" required minlength="3" maxlength="20" size="10">
     <br><br>
 
-    <!-- +----------------------------+  -->
-    <!-- |   Acteur nom :  [   \/]    |  -->
-    <!-- +----------------------------+  -->
+    <!-- +-------------------------------+  -->
+    <!-- |    Acteur nom :  [   \/]      |  -->
+    <!-- +-------------------------------+  -->
     <label for="acteurNom">Acteur nom:</label>
     <select name="acteurNom" id="acteurNom"><?php 
         // boucle pour affiche les acteur un par un
@@ -32,9 +32,9 @@
         }?>
     </select><br><br>
 
-    <!-- +----------------------------+  -->
-    <!-- |  Acteur prenom :  [   \/]  |  -->
-    <!-- +----------------------------+  -->
+    <!-- +-------------------------------+  -->
+    <!-- |    Acteur prenom :  [   \/]   |  -->
+    <!-- +-------------------------------+  -->
     <label for="acteurPrenom">Acteur prenom:</label>
     <select name="acteurPrenom" id="acteurPrenom"><?php 
         // boucle pour affiche les acteur un par un
@@ -42,9 +42,9 @@
             <option><?= $acteur["prenom"]?></option><?php 
         }?>
     </select><br><br>
-    <!-- +----------------------------+  -->
-    <!-- |    [ ajouter personne ]    |-->
-    <!-- +----------------------------+  -->
+    <!-- +-------------------------------+  -->
+    <!-- |    [ ajouter personne ]       |  -->
+    <!-- +-------------------------------+  -->
     <input type="submit" value="ajouter personne" >
 
 </form>
