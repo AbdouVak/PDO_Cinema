5b44a69e093738269ad3dfc4dc982b1232dec621
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-06 14:12:25 +0200

    homePage: déplacement de page listFilm a ici pour

--- view/homePage.php
+++ view/homePage.php
@@ -1,7 +1,19 @@
 <?php ob_start();?>
 
-<p> Site regroupant plusieur film</p></br>
-<p> Permet de trier les film en fonction du genre / realisateur / acteur / date de srotie / </p>
+<p> Il y a <?= $requete->rowCount()?> films</p>
+
+
+<div><?php
+    // boucle pour recupere les info du film. Chaque boucle est un film different
+    foreach($requete->fetchAll() as $film){ 
+        ?>
+        <div>
+            <p><?= $film["titre"] ?>-<?= $film["anneeSortieFrance"]?> </p>
+            <!-- quand no va appuyer sur l'image redirige vers l'index avec l'action description et id_film du film de la boucle-->
+            <?= "<a href='index.php?action=description&id= ".$film['id_film']." '><img src='public/img/test_img.jpg' width='200'></a>"?>
+        </div>
+<?php } ?></div>
+
 <?php
 $titre = "Acceuil";
 $titreSecondaire = "Acceuil";
