ab43ea972ea2204157cec057df066877129a56d8
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-07 16:31:08 +0200

    homePage: affiche un poster en fonction de si le film en a 1 ou pas

--- view/homePage.php
+++ view/homePage.php
@@ -2,16 +2,21 @@
 
 <p> Il y a <?= $requete->rowCount()?> films</p>
 
-
 <div><?php
     // boucle pour recupere les info du film. Chaque boucle est un film different
-    foreach($requete->fetchAll() as $film){ 
+    foreach($requete->fetchAll() as $film){
         ?>
         <div>
             <p><?= $film["titre"] ?>-<?= $film["anneeSortieFrance"]?> </p>
             <!-- quand no va appuyer sur l'image redirige vers l'index avec l'action description et id_film du film de la boucle-->
-            <?= "<a href='index.php?action=description&id= ".$film['id_film']." '><img src='public/img/test_img.jpg' width='200'></a>"?>
-        </div>
+            
+            <?php // affiche une img si le film n'a pas d'affiche
+            if($film['affiche'] == NULL){
+                ?><a href='index.php?action=description&id=<?= $film['id_film'] ?>'> <img src="public/img/no-cover.png" width='200'></a><br><?php 
+            }else{// sinon on met l'affiche
+                ?><a href='index.php?action=description&id=<?= $film['id_film'] ?>'> <img src='public/img/<?= $film['affiche']?>' width="200"></a><br><?php 
+            } ?>
+            </div>
 <?php } ?></div>
 
 <?php
