b910b79f6c6ba6b8142900dcb32ccb11ff5c72f3
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-07 11:03:04 +0200

    CinemaController: chanegment des filtre + ajout requête récupére tout les rôle de la BDD

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -119,6 +119,14 @@ class CinemaController{
         $castingStatement->execute();
         $requeteActeur = $castingStatement->fetchAll();
 
+        $sqlRole = "
+        SELECT nomPersonnage
+        FROM role";
+        
+        $roleStatement = $pdo->prepare($sqlRole);
+        $roleStatement->execute();
+        $requeteRole = $roleStatement->fetchAll();
+
         require "view/ajouterCastingPage.php"; // redirige vers la page ajouterCastingPage
     }
 
@@ -151,10 +159,16 @@ class CinemaController{
     // ajoue a la bdd une personne
     public function ajouterPersonne() {
         $pdo = Connect::seConnecter();
+
+        $nom = filter_var($_POST['nom'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $prenom = filter_var($_POST['prenom'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $sexe = filter_var($_POST['sexe'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $dateNaissance = filter_var($_POST['dateNaissance'], FILTER_SANITIZE_NUMBER_INT);
+
         // requête pour ajouter une personne à la bdd 
         $sqlPersonne = "
         INSERT INTO `cinema`.`personne` ( `nom`, `prenom`, `sexe`, `dateNaissance`) 
-        VALUES ('".htmlspecialchars($_POST['nom'], ENT_QUOTES)."', '".htmlspecialchars($_POST['prenom'], ENT_QUOTES)."', '".htmlspecialchars($_POST['sexe'], ENT_QUOTES)."', '".htmlspecialchars($_POST['dateNaissance'], ENT_QUOTES)."');
+        VALUES ('".$nom."', '".$prenom ."', '".$sexe."', '".$dateNaissance."');
         ";
 
         $personnesStatement = $pdo->prepare($sqlPersonne);
@@ -167,8 +181,8 @@ class CinemaController{
             INSERT INTO `cinema`.`acteur`(`id_personne`)
             SELECT id_personne 
             FROM personne
-            WHERE nom = '".htmlspecialchars($_POST['nom'], ENT_QUOTES)."';
-            AND prenom = '".htmlspecialchars($_POST['prenom'], ENT_QUOTES)."';
+            WHERE nom = '".$nom."';
+            AND prenom = '".$prenom."';
             ";
 
             $acteurStatement = $pdo->prepare($sqlActeur);
@@ -179,8 +193,8 @@ class CinemaController{
             INSERT INTO `cinema`.`realisateur`(`id_personne`)
             SELECT id_personne 
             FROM personne
-            WHERE nom = '".htmlspecialchars($_POST['nom'], ENT_QUOTES)."';
-            AND prenom = '".htmlspecialchars($_POST['prenom'], ENT_QUOTES)."';
+            WHERE nom = '".$nom."';
+            AND prenom = '".$prenom."';
             ";
 
             $realStatement = $pdo->prepare($sqlReal);
@@ -193,6 +207,12 @@ class CinemaController{
     // ajoute ajoute un acteur avec son role d'un dans la bdd
     public function ajouterCasting() {
         $pdo = Connect::seConnecter();
+
+        $film = filter_var($_POST['film'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $role = filter_var($_POST['role'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $acteurPrenom = filter_var($_POST['acteurPrenom'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $acteurNom = filter_var($_POST['acteurNom'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
         // requête pour ajouter a la table jouer l'acteur avec son role dans un film
         $sqlCasting = "
         INSERT INTO `cinema`.`jouer` (`id_film`, `id_acteur`, `id_role`)
@@ -202,21 +222,21 @@ class CinemaController{
 
         HAVING idFilm IN (
             SELECT id_film 
-            FROM film 
-            WHERE titre = '".htmlspecialchars($_POST['film'], ENT_QUOTES)."')
+        FROM film 
+            WHERE titre = '$film')
 
         AND idRole IN (
-            SELECT id_role 
-            FROM role,acteur 
-            WHERE role.id_role
-            AND nomPersonnage = '".htmlspecialchars($_POST['role'], ENT_QUOTES)."')
-            
+        SELECT id_role 
+        FROM role,acteur 
+        WHERE role.id_role
+        AND nomPersonnage = '$role')
+                    
         AND idActeur IN (
-            SELECT id_acteur 
+        SELECT id_acteur 
             FROM acteur,personne 
-            WHERE acteur.id_personne = personne.id_personne
-            AND personne.prenom = '".htmlspecialchars($_POST['acteurPrenom'], ENT_QUOTES)."'
-            AND personne.nom = '".htmlspecialchars($_POST['acteurNom'], ENT_QUOTES)."')
+        WHERE acteur.id_personne = personne.id_personne
+        AND personne.prenom = '$acteurPrenom'
+        AND personne.nom = '$acteurNom')
         ";
 
         $castingStatement = $pdo->prepare($sqlCasting);
@@ -228,10 +248,11 @@ class CinemaController{
     // ajoute ajoute un acteur avec son role d'un dans la bdd
     public function ajouterRole() {
         $pdo = Connect::seConnecter();
+        $role = filter_var($_POST['role'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         // requete pour ajouter un role
         $sqlRole= "
         INSERT INTO `cinema`.`role` (`nomPersonnage`) 
-        VALUES ('".htmlspecialchars($_POST['role'], ENT_QUOTES)."');
+        VALUES ('".$role."');
         ";
         $roleStatement = $pdo->prepare($sqlRole);
         $roleStatement->execute();
@@ -239,29 +260,25 @@ class CinemaController{
     }
 
     // ajoute ajoute un acteur avec son role d'un dans la bdd
-    public function ajouterGenre() {
-        $pdo = Connect::seConnecter();
-        // requete pour ajouter un role
-        $sqlGenre= "
-        INSERT INTO `cinema`.`genre` (`genreLibelle`) 
-        VALUES ('".htmlspecialchars($_POST['genre'], ENT_QUOTES)."');
-        ";
-        $genreStatement = $pdo->prepare($sqlGenre);
-        $genreStatement->execute();
-        require "view/ajouterCastingPage.php";// redirige vers la page ajouterCastingPage
-    }
-
-    // ajoute ajoute un acteur avec son role d'un dans la bdd
     public function ajouterFilm() {
+
         $pdo = Connect::seConnecter();
+
+        $titre = filter_var($_POST['titre'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $anneeSortie = filter_var($_POST['anneeSortie'], FILTER_SANITIZE_NUMBER_INT);
+        $duree = filter_var($_POST['duree'], FILTER_SANITIZE_NUMBER_INT);
+        $prenomReal = filter_var($_POST['prenomReal'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $nomReal = filter_var($_POST['nomReal'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $genre = filter_var($_POST['genre'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
         // requete pour ajouter un role
         $sqlGenre= "
         INSERT INTO `cinema`.`film` (`titre`, `anneeSortieFrance`, `duree`, `id_realisateur`) 
-        SELECT '".htmlspecialchars($_POST['titre'], ENT_QUOTES)."', '".htmlspecialchars($_POST['anneeSortie'], ENT_QUOTES)."', '".htmlspecialchars($_POST['duree'], ENT_QUOTES)."',id_realisateur
+        SELECT '".$titre."', '".$anneeSortie."', '".$duree."',id_realisateur
         FROM realisateur,personne
         WHERE realisateur.id_personne = personne.id_personne 
-        AND nom = '".htmlspecialchars($_POST['realisateurNom'], ENT_QUOTES)."'
-        AND prenom = '".htmlspecialchars($_POST['realisateurPrenom'], ENT_QUOTES)."'
+        AND nom = '".$prenomReal."'
+        AND prenom = '".$nomReal."'
         ";
 
         $genreStatement = $pdo->prepare($sqlGenre);
@@ -269,7 +286,7 @@ class CinemaController{
 
         $sqlGenre= "
         INSERT INTO `cinema`.`genre` (`genreLibelle`) 
-        VALUES ('".htmlspecialchars($_POST['genre'], ENT_QUOTES)."');
+        VALUES ('".$genre."');
         ";
         $genreStatement = $pdo->prepare($sqlGenre);
         $genreStatement->execute();
@@ -279,8 +296,8 @@ class CinemaController{
         INSERT INTO `cinema`.`genrefilm` (`id_film`, `id_genre`)
         SELECT film.id_film,genre.id_genre
         FROM film,genre
-        WHERE film.titre = '".htmlspecialchars($_POST['titre'], ENT_QUOTES)."'
-        AND genre.genreLibelle ='".htmlspecialchars($_POST['genre'], ENT_QUOTES)."'
+        WHERE film.titre = '".$titre."'
+        AND genre.genreLibelle ='".$genre."'
         ";
 
         $genreFilmStatement = $pdo->prepare($sqlGenreFilm);
