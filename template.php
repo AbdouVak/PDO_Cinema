c2eea0cf72cce6a62e7398ad0000c0586c698a75
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-08 08:48:05 +0200

    template: déplacement du fichier

---
+++ template.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $titre ?></title>
+</head>
+
+<body>
+    <nav>
+        <a href="index.php?action=homePage">Accueil</a>
+        <a href="index.php?action=ajouterPersonnePage">ajouter personne</a>
+        <a href="index.php?action=ajouterCastingPage">ajouter casting</a>
+        <a href="index.php?action=ajouterFilmPage">ajouter film</a>
+    </nav>
+    <div>
+        <main>
+            <div id="contenue">
+                <h1>PDO cinema</h1>
+                <h2><?= $titreSecondaire ?></h2>
+                <?= $contenu  ?>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
