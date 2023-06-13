83c01c925975bb138576ca8c5e25c3bf0cd65e99
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-13 15:06:23 +0200

    descModificationPage: ajout des input et formulaire pour chaque info du film a modifier

--- view/descModificationPage.php
+++ view/descModificationPage.php
@@ -59,9 +59,15 @@ foreach($requeteFilm as $desc){ ?>
         
 
         <?php foreach($requeteCasting as $casting){?>
-            <form action="index.php?action=modifierActeur&id=<?=$idFilm?>" method="POST" id="modifier acteur">
+            <form action="index.php?action=modifierCasting&id=<?=$idFilm?>" method="POST" id="modifier acteur">
             <label for="role">Role :</label>
-            <input type="text" name="role" value='<?= $casting['nomPersonnage'] ?>' required minlength="3" maxlength="20" size="10">
+            <input type="text" name="role" list="roleList" value='<?= $casting['nomPersonnage'] ?>' required minlength="3" maxlength="20" size="10">
+            <datalist id="roleList"><?php 
+                // boucle pour affiche les acteur un par un
+                foreach($requeteRole as $role){?>
+                    <option> <?= $role["nomPersonnage"]?></option>
+                <?php }?>
+            </datalist>
             <br><br>
 
             <label for="acteurPrenom">Acteur prenom:</label>
@@ -86,12 +92,58 @@ foreach($requeteFilm as $desc){ ?>
         <?php }?>
         
         
+        <h2>Année de sortie</h2>
+        <!-- +-----------------------------------------+  -->
+        <!-- |     titre :  [     ]                    |  -->
+        <!-- +-----------------------------------------+  -->
+        <form action="index.php?action=modifierAnnee&id=<?=$idFilm?>" method="POST" id="modifier année">
+            Année Sortie : 
+            <input type="text" name="anneeSortie" value='<?= $desc['anneeSortieFrance'] ?>' required minlength="3" maxlength="20" size="10"> <br></br>
+            <input type="submit" value="modifier année" >
+        </form><br></br>
 
+        <h2>Genre</h2>
+        <!-- +-----------------------------------------+  -->
+        <!-- |     Genre :                             |  -->
+        <!-- |     [     ]                             |  -->
+        <!-- +-----------------------------------------+  -->
+        <form action="index.php?action=modifierGenre&id=<?=$idFilm?>" method="POST" id="modifier genre">
+            <?php foreach($requeteGenreFilm as $genreFilm){?><br>
+                <input type="text" name="genre" list="genreList" value='<?= $genreFilm['genreLibelle']?>' required minlength="3" maxlength="20" size="10">
+                <datalist id="genreList"><?php 
+                    // boucle pour affiche les acteur un par un
+                    foreach($requeteGenre as $genre){?>
+                        <option> <?= $genre["genreLibelle"]?></option>
+                    <?php }?>
+                </datalist><br><br>
+            <?php }?>
+            <input type="submit" value="modifier genre" >
+        </form><br></br>
 
-
-
-
-
+        <h2>Synopsis</h2>
+        <!-- +-----------------------------------------+  -->
+        <!-- |     Synopsis                            |  -->
+        <!-- |     +--------------------+              |  -->
+        <!-- |     |                    |              |  -->
+        <!-- |     |                    |              |  -->
+        <!-- |     +--------------------+              |  -->
+        <!-- +-----------------------------------------+  -->
+        <form action="index.php?action=modifierSynopsis&id=<?=$idFilm?>" method="POST" id="modifier synopsis">
+        <label for="synopsis">Synopsis (optionnel) :</label><br>
+        <textarea name="synopsis" cols="40" rows="5"></textarea>
+        <br><br>
+        <input type="submit" value="modifier synopsis" >
+        </form>
+        <h2>Poster</h2>
+        <!-- +----------------------------------------+  -->
+        <!-- |   Affiche :  [      ]                  |  -->
+        <!-- +----------------------------------------+  -->
+        <form action="index.php?action=modifierAffiche&id=<?=$idFilm?>" method="POST" id="modifier affiche" enctype="multipart/form-data">
+        <label for="file">affiche :</label><br>
+        <input type="file" name="file">
+        <br><br>
+        <input type="submit" value="modifier affiche" >
+        </form>
     </div><?php }?>
 
 </form> 
