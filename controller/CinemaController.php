645931fb8293f16b0a16ef7c23511342605452e6
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-14 11:54:47 +0200

    cinController: déplacement des commentaire dans  fonction correspondant + création meth listeActeurs

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -6,15 +6,16 @@ use Model\Connect;
 class CinemaController{
 
 
-    /* Methode listFilms()
+    
+    public function listFilms() {
+        /* Methode listFilms()
         1. Cet methode possede une requête SQl qui va chercher tout les titre 
         dans la base de données. 
 
         2. Puis les données seront stocke dans la variable $requete 
 
         3. Finalement il va rediriger vers le homePage.php
-    */
-    public function listFilms() {
+        */
         $pdo = Connect::seConnecter(); // Permet de se connecter a la base de données
         
         // \/ Etape 2     \/ Etape 1
@@ -27,10 +28,9 @@ class CinemaController{
         // Etape 3.
         require "view/homePage.php"; 
     }
-
- 
-
-    /* Methode description($id)
+    
+    public function description($id){
+        /* Methode description($id)
         1. On stocke $id dans $idFilm pour permettre dans la page description de 
         faire un bouton modifier qui va servir a la page descModification 
         De savoir qu'elle film doit être modifier
@@ -42,9 +42,8 @@ class CinemaController{
         Elle va récupérer tout les info sur le film précis graçe à l'$id
 
         4. Stocke tout dans $requeteFilm
-    */
-    public function description($id){
-        
+        */
+
         $idFilm = $id;// Etape 1.
         $ctrlTake = new ControllerTakeDB();
         $pdo = Connect::seConnecter();// Permet de se connecter a la base de données
@@ -61,8 +60,6 @@ class CinemaController{
         
         require "view/descriptionPage.php"; // va rediriger vers la page description
     }
-
-
 
     public function descModificationPage($id){
         $idFilm = $id;// Etape 1.
@@ -87,23 +84,19 @@ class CinemaController{
 
         require "view/descModificationPage.php"; // va rediriger vers la page description
     }
-
-
-
-    /* Methode ajouterPersonnePage() 
+    
+    public function ajouterPersonnePage() {  
+        /* Methode ajouterPersonnePage() 
         redirige a la page d'ajout d'un personnage 
-    */
-    public function ajouterPersonnePage() {   
+        */ 
         require "view/ajouterPersonnePage.php"; // va rediriger vers la page ajouterPersonne
     }
 
-
-
-    /* Methode ajouterCastingPage()
+    public function ajouterCastingPage() {
+         /* Methode ajouterCastingPage()
         1. On envoie une requête SQL à la bdd
         2. On stocke la réponse d'une requête SQL dans une variale
-    */
-    public function ajouterCastingPage() {
+        */
         $pdo = Connect::seConnecter(); // On se connect a la base de données
         $ctrlTake = new ControllerTakeDB();
         // \/ Etape 2     \/ Etape 1 (la requête recupere tout les titre dans la bdd) 
@@ -128,9 +121,8 @@ class CinemaController{
     }
 
 
-
-    /* Methode ajouterFilmPage()*/
     public function ajouterFilmPage() {
+        /* Methode ajouterFilmPage()*/
         $pdo = Connect::seConnecter(); // On se connect a la base de données
         $ctrlTake = new ControllerTakeDB();
 
@@ -146,40 +138,41 @@ class CinemaController{
         WHERE personne.id_personne = realisateur.id_personne");
         // \/ Etape 2     \/ Etape 1 (la requête recupere tout les nom des genres dans la bdd) 
         $requeteGenre = $ctrlTake -> recuperGenre();
-
+        
         require "view/ajouterFilmPage.php";// redirige vers la page ajouterFilmPage
     }
 
     public function filmographie($id,$metier) {
         $pdo = Connect::seConnecter(); // On se connect a la base de données
         if($metier == 'act'){
-            $sqlTitre = "
+            $requeteFilm = $pdo->query("
             SELECT titre,anneeSortieFrance,nomPersonnage,affiche
             FROM role,jouer,film,acteur
             WHERE role.id_role = jouer.id_role
             AND film.id_film = jouer.id_film
             AND acteur.id_acteur = jouer.id_acteur
-            AND acteur.id_acteur = $id";
+            AND acteur.id_acteur = $id");
             
-            $titreStatement = $pdo->prepare($sqlTitre);
-            $titreStatement->execute();
-            $requeteFilm = $titreStatement->fetchAll(); 
         }elseif($metier == 'rea'){
-            $sqlTitre = "
+            $requeteFilm = $pdo->query("
             SELECT titre,anneeSortieFrance,affiche
             FROM film
-            WHERE film.id_realisateur = $id";
-            
-            $titreStatement = $pdo->prepare($sqlTitre);
-            $titreStatement->execute();
-            $requeteFilm = $titreStatement->fetchAll();
+            WHERE film.id_realisateur = $id");
         }
-        
-        
-        
 
         require "view/filmographiePage.php";// redirige vers la page ajouterFilmPage
     }
 
+    public function listeActeurPage() {
+        $ctrlTake = new ControllerTakeDB;
+        $pdo = Connect::seConnecter(); // On se connect a la base de données
+        $listeActeur = $ctrlTake ->recuperActeur();
 
+        echo 'LISTE DES ACTEURS<br>';
+        foreach($listeActeur as $acteur){
+            echo '#'.$acteur['nom'].'<br>';
+        }
+
+        
+    }
 }
