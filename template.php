0603d764fbdc07420cd0eb1e7891f4c2e031033f
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-14 16:56:47 +0200

    template: ajout de class css

--- template.php
+++ template.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css" />
     <title><?= $titre ?></title>
 </head>
 
 <body>
     <nav>
-        <a href="index.php?action=homePage">Accueil</a>
-        <a href="index.php?action=ajouterPersonnePage">ajouter personne</a>
-        <a href="index.php?action=ajouterCastingPage">ajouter casting</a>
-        <a href="index.php?action=ajouterFilmPage">ajouter film</a>
-        <a href="index.php?action=listeActeurPage">liste Acteur</a>
+        <a class="lienNavBar" href="index.php?action=homePage">Accueil</a>
+        <a class="lienNavBar" href="index.php?action=listeActeurPage">liste Acteur</a>
+        <a class="lienNavBar" href="index.php?action=listeReaPage">liste Réalisateur</a>
+        <a class="lienNavBar" href="index.php?action=listeGenrePage">liste Genre</a>
+        <a class="lienNavBar" href="index.php?action=ajouterPersonnePage">ajouter personne</a>
+        <a class="lienNavBar" href="index.php?action=ajouterFilmPage">ajouter film</a>
+        <a class="lienNavBar" href="index.php?action=ajouterCastingPage">ajouter casting</a>
     </nav>
     <div>
         <main>
