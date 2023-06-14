f3f6e1dc192ee2fb16b646d920cf70bf2a044091
AbdouVak <abdou.vakouiev@gmail.com>
2023-06-14 16:58:59 +0200

    listeActeurPage: liste les acteur dans l'odre alphabetique

--- view/listeActeurPage.php
+++ view/listeActeurPage.php
@@ -1,5 +1,17 @@
-<?php ob_start();?>
+<?php ob_start(); 
 
+
+foreach($alphabet as $lettres){?>
+    <div class="alphabetBar"><h3>Lettres <?= $lettres?></h3></div>
+    
+    <div class="personneContaineur">
+        <?php foreach($requeteActeur as $acteur){
+            if($acteur['nom'][0] == $lettres){?>
+                    <a class="lienPersonne" href="index.php?action=filmographieAct&id=<?= $acteur['id_acteur']?>"><?= $acteur['nom']?> <?= $acteur['prenom']?><br></a><?php 
+            } 
+        }?>
+    </div><?php
+}?>
 
 
 <?php
